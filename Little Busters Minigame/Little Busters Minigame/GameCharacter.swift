bddf2256ed619a7104effdb48b52ba3bb29cf25b
Silver Wei <silveracgal@gmail.com>
2016-04-13 11:25:04 +0800

    完成理树挥棒动作

--- Little Busters Minigame/Little Busters Minigame/GameCharacter.swift
+++ Little Busters Minigame/Little Busters Minigame/GameCharacter.swift
@@ -11,49 +11,61 @@ import UIKit
 
 class GameCharacter{
     
+    struct Unit {
+        var character: Character
+        var image: [UIImage]
+    }
 
     struct Character{
         var image: UIImage
         var point: CGPoint
+        var status: Int
+        var imageNumber: Int
     }
     
     //直枝 理樹
-    var Naoe_Riki = Character(image: UIImage(named: "NaoeRiki")!,
-        point: CGPoint(x:  GameBackground().Baseballfield.anchorPoint.x, y: GameBackground().Baseballfield.anchorPoint.y - 100)
+    var Naoe_Riki_Attribute = Character(image: UIImage(named: "NaoeRiki")!,
+        point: CGPoint(x:  GameBackground().Baseballfield.anchorPoint.x, y: GameBackground().Baseballfield.anchorPoint.y - 100),
+        status: 0,
+        imageNumber: 0
     )
     func Naoe_Riki_Array() -> [UIImage]{
         var Naoe_Riki_Array:[UIImage] = []
-        let Naoe_Riki_H = Naoe_Riki.image.size.height / 5
-        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki, x: 0, y: 0, w: 97, h: Naoe_Riki_H))
-        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki, x: 0, y: Naoe_Riki_H, w: 97, h: Naoe_Riki_H))
-        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki, x: 0, y: Naoe_Riki_H * 2, w: 97, h: Naoe_Riki_H))
-        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki, x: 0, y: Naoe_Riki_H * 3, w: 97, h: Naoe_Riki_H))
-        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki, x: 0, y: Naoe_Riki_H * 4, w: 97, h: Naoe_Riki_H))
+        let Naoe_Riki_H = Naoe_Riki_Attribute.image.size.height / 5
+        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki_Attribute, x: 0, y: 0, w: 97, h: Naoe_Riki_H))
+        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki_Attribute, x: 0, y: Naoe_Riki_H, w: 97, h: Naoe_Riki_H))
+        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki_Attribute, x: 0, y: Naoe_Riki_H * 2, w: 97, h: Naoe_Riki_H))
+        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki_Attribute, x: 0, y: Naoe_Riki_H * 3, w: 97, h: Naoe_Riki_H))
+        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki_Attribute, x: 0, y: Naoe_Riki_H * 4, w: 97, h: Naoe_Riki_H))
         
-        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki, x: 97, y: 0, w: 97, h: Naoe_Riki_H))
-        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki, x: 97, y: Naoe_Riki_H, w: 97, h: Naoe_Riki_H))
-        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki, x: 97, y: Naoe_Riki_H * 2, w: 97, h: Naoe_Riki_H))
-        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki, x: 97, y: Naoe_Riki_H * 3, w: 97, h: Naoe_Riki_H))
-        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki, x: 97, y: Naoe_Riki_H * 4, w: 97, h: Naoe_Riki_H))
+        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki_Attribute, x: 97, y: 0, w: 97, h: Naoe_Riki_H))
+        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki_Attribute, x: 97, y: Naoe_Riki_H, w: 97, h: Naoe_Riki_H))
+        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki_Attribute, x: 97, y: Naoe_Riki_H * 2, w: 97, h: Naoe_Riki_H))
+        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki_Attribute, x: 97, y: Naoe_Riki_H * 3, w: 97, h: Naoe_Riki_H))
+        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki_Attribute, x: 97, y: Naoe_Riki_H * 4, w: 97, h: Naoe_Riki_H))
         
-        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki, x: 195, y: 0, w: 79, h: Naoe_Riki_H))
-        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki, x: 195, y: Naoe_Riki_H, w: 79, h: Naoe_Riki_H))
-        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki, x: 195, y: Naoe_Riki_H * 2, w: 79, h: Naoe_Riki_H))
-        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki, x: 195, y: Naoe_Riki_H * 3, w: 79, h: Naoe_Riki_H))
-        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki, x: 195, y: Naoe_Riki_H * 4, w: 79, h: Naoe_Riki_H))
+        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki_Attribute, x: 195, y: 0, w: 79, h: Naoe_Riki_H))
+        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki_Attribute, x: 195, y: Naoe_Riki_H, w: 79, h: Naoe_Riki_H))
+        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki_Attribute, x: 195, y: Naoe_Riki_H * 2, w: 79, h: Naoe_Riki_H))
+        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki_Attribute, x: 195, y: Naoe_Riki_H * 3, w: 79, h: Naoe_Riki_H))
+        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki_Attribute, x: 195, y: Naoe_Riki_H * 4, w: 79, h: Naoe_Riki_H))
         
-        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki, x: 195 + 79, y: 0, w: 79, h: Naoe_Riki_H))
-        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki, x: 195 + 79, y: Naoe_Riki_H, w: 79, h: Naoe_Riki_H))
-        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki, x: 195 + 79, y: Naoe_Riki_H * 2, w: 79, h: Naoe_Riki_H))
-        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki, x: 195 + 79, y: Naoe_Riki_H * 3, w: 79, h: Naoe_Riki_H))
-        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki, x: 195 + 79, y: Naoe_Riki_H * 4, w: 79, h: Naoe_Riki_H))
+        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki_Attribute, x: 195 + 79, y: 0, w: 79, h: Naoe_Riki_H))
+        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki_Attribute, x: 195 + 79, y: Naoe_Riki_H, w: 79, h: Naoe_Riki_H))
+        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki_Attribute, x: 195 + 79, y: Naoe_Riki_H * 2, w: 79, h: Naoe_Riki_H))
+        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki_Attribute, x: 195 + 79, y: Naoe_Riki_H * 3, w: 79, h: Naoe_Riki_H))
+        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki_Attribute, x: 195 + 79, y: Naoe_Riki_H * 4, w: 79, h: Naoe_Riki_H))
         
-        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki, x: 195 + 79 * 2, y: 0, w: 79, h: Naoe_Riki_H))
-        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki, x: 195 + 79 * 2, y: Naoe_Riki_H, w: 79, h: Naoe_Riki_H))
+        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki_Attribute, x: 195 + 79 * 2, y: 0, w: 79, h: Naoe_Riki_H))
+        Naoe_Riki_Array.append(ImageInterception(Naoe_Riki_Attribute, x: 195 + 79 * 2, y: Naoe_Riki_H, w: 79, h: Naoe_Riki_H))
         
         return Naoe_Riki_Array
     }
-    
+    enum Naoe_Riki_status{
+        case NR_static //静止
+        case NR_Swing //挥动
+    }
+
     func ImageInterception(character: Character, x: CGFloat, y: CGFloat, w: CGFloat,h: CGFloat) -> UIImage {
         return UIImage(CGImage: CGImageCreateWithImageInRect(character.image.CGImage, CGRectMake(x, y, w, h))!)
     }
--- Little Busters Minigame/Little Busters Minigame/KittyBaseballGame.swift
+++ Little Busters Minigame/Little Busters Minigame/KittyBaseballGame.swift
@@ -13,6 +13,11 @@ class KittyBaseballGame: SKScene {
     let GameView = SKNode()
     let Baseballfield = GameBackground().Baseballfield
     
+    var Naoe_Riki = GameCharacter.Unit(character: GameCharacter().Naoe_Riki_Attribute,
+        image: GameCharacter().Naoe_Riki_Array()
+    )
+    var Characterfront = SKSpriteNode(texture: SKTexture(image: GameCharacter().Naoe_Riki_Array()[0]))
+    
     enum Layers: CGFloat{
         case Baseballfield //棒球场背景
         case shadow //阴影
@@ -26,29 +31,53 @@ class KittyBaseballGame: SKScene {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         addChild(GameView)
-        ShowBaseballfield()//显示棒球场背景
-        ShowCharacterfront()//显示在前面的人物
+        Show_Baseballfield()//显示棒球场背景
+        Show_Characterfront()//显示在前面的人物
     }
     
-    func ShowBaseballfield(){
+    func Show_Baseballfield(){
         Baseballfield.position = CGPoint(x: size.width / 2, y: size.height / 2)
         Baseballfield.zPosition = Layers.Baseballfield.rawValue
         GameView.addChild(Baseballfield)
     }
     
-    func ShowCharacterfront(){
-        let Naoe_Riki = SKSpriteNode(texture: SKTexture(image: GameCharacter().Naoe_Riki_Array()[20]))
-        Naoe_Riki.position = GameCharacter().Naoe_Riki.point
-        Naoe_Riki.zPosition = Layers.Characterfront.rawValue
-        Baseballfield.addChild(Naoe_Riki)
+    func Show_Characterfront(){
+        Characterfront.position = Naoe_Riki.character.point
+        Characterfront.zPosition = Layers.Characterfront.rawValue
+        Baseballfield.addChild(Characterfront)
     }
     
     //MARK: 点击
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        
+        Naoe_Riki.character.status = GameCharacter.Naoe_Riki_status.NR_Swing.hashValue
     }
     
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        Status_Characterfront() //前面的人物状态
     }
+    
+    func Status_Characterfront(){
+        if(Naoe_Riki.character.status != GameCharacter.Naoe_Riki_status.NR_static.hashValue){
+            if(Naoe_Riki.character.imageNumber.hashValue == 9){
+                Naoe_Riki.character.imageNumber = 0
+                Characterfront.runAction(SKAction.setTexture(SKTexture(image: Naoe_Riki.image[Naoe_Riki.character.imageNumber])))
+                Naoe_Riki.character.status = GameCharacter.Naoe_Riki_status.NR_static.hashValue
+                return
+            }
+
+            if((actionForKey("Characterfront_SwingAction")) != nil){
+                return
+            }
+            let Characterfront_Start = SKAction.runBlock(Characterfront_Swing)
+            let TimeInterval = SKAction.waitForDuration(NSTimeInterval(0.05))
+            let Characterfront_SwingAction = SKAction.sequence([Characterfront_Start,TimeInterval])
+            runAction(Characterfront_SwingAction, withKey: "Characterfront_SwingAction")
+        }
+    }
+    
+    func Characterfront_Swing(){
+        Characterfront.runAction(SKAction.setTexture(SKTexture(image: Naoe_Riki.image[Naoe_Riki.character.imageNumber++])))
+    }
+    
 }
